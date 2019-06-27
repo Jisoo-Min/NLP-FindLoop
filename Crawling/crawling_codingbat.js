7236092685594a9efcc4a2c5305fddeb570701b4
Jung Jinwook <jinwook1240@gmail.com>
2019-06-27 13:15:28 -0700

    Update CodingBat Crawling Code
    remained Issues:
    there is 7 none data spaces on top of .csv file
    sometimes the code dies
    
    warnings:
    do not open .csv file with excel, nodepad, etc while code working
    
    Related with https://github.com/jiisoo/NLP-FindLoop/issues/1

--- Crawling/crawling_codingbat.js
+++ Crawling/crawling_codingbat.js
@@ -5,13 +5,20 @@ const probAndLoop = [
   ['Warmup-1',0],
   ['Warmup-2',1],
   ['String-1',1],
+  ['String-2',1],
+  ['String-3',2],
   ["Array-1",0],
+  ["Array-2",1],
+  ["Array-3",2],
   ['Map-1',0],
   ['Map-2',1],
   ['Logic-1',0],
   ['Logic-2',0],
   ['Functional-1',0],
-  ['Functional-2',0]
+  ['Functional-2',0],
+  ["AP-1",0],
+  ["Recursion-1",1],
+  ["Recursion-2",1],
 ];
 //Logic1 및 Logic2는 Loop가 있는지 없는지 재확인 필요.
 //만약 Loop가 있을 시 해당 항목을 -1로 변경 후 수작업
@@ -24,7 +31,7 @@ const crawl_prob = async function(href,loop){
     }, (err1,res1,body1)=>{
      let $1 = cheerio.load(body1);
      let speech = $1('.max2').text();
-     console.log(speech);
+     //console.log(speech);
      resolve({'speech':speech, 'loop':loop});
     });
   });
@@ -36,18 +43,28 @@ const crawl_chapter = async function(filename,loop){
      }, (err,res,body)=>{
      if(err) return console.error(err);
      let $ = cheerio.load(body);
-     let links = $('.indent > table:nth-child(12) > tbody:nth-child(1) > tr > td > a').toArray();
+     let links = $('table > tbody > tr > td > a').toArray();
      let promises = [];
      let result = [];
       for(let i in links){
-          //console.log(links[i].attribs.href);
-          promises[i] = crawl_prob(links[i].attribs.href,loop);
-          promises[i].then((res)=>{
-            result.push(res);
-          });
+          console.log(links[i].attribs.href);
+          try{
+          console.log(i);
+            promises[i] = crawl_prob(links[i].attribs.href,loop);
+            promises[i].then((res)=>{
+              result[i] = res;
+            });
+          }
+          catch{
+          console.log(i+'err');
+            promises[i] = crawl_prob(links[i].attribs.href,loop);
+            promises[i].then((res)=>{
+              result[i] = res;
+            });
+          }
       }
       Promise.all(promises).then(()=>{
-        console.log(result);
+        //console.log(result);
 
         const createCsvWriter = require('csv-writer').createObjectCsvWriter;
         const csvWriter = createCsvWriter({
@@ -58,11 +75,11 @@ const crawl_chapter = async function(filename,loop){
           ]
         });
         csvWriter.writeRecords(result);
-        fs.writeFile('dataset_'+filename+'.json',result,function(err){
+        /*fs.writeFile('dataset_'+filename+'.json',result,function(err){
           if(err){
             console.log(err);
           }
-        });
+        });*/
       });
     });
   });
